30a70c22857663170ede8dbe957a88b6ed6dd11f
Carina Klömpges <carinawerner25@gmail.com>
2023-11-27 14:17:41 +0100

    funktioniert alles, nur Styling bei 3_1 bis jetzt nur bei der home fertig

---
+++ level 3_1/index.js
@@ -0,0 +1,28 @@
+import http from "http";
+import fs from "fs";
+
+const sendFile = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.end("error");
+    }
+    res.end(data);
+  });
+};
+
+const requestHandler = (req, res) => {
+  console.log("Ein neuer Request: ", req.url, req.method);
+
+  if (req.url === "/") {
+    sendFile("./assets/home.html", res);
+  } else {
+    const filePath = "./assets" + req.url;
+    sendFile(filePath, res);
+  }
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(9898, () => {
+  console.log("huhu, bin da");
+});
